58b5c7860737e4113f0aeda2ba09868f5ab35f17
Harikrishnan001 <harikrishnanasok@gmail.com>
2022-09-14 15:07:47 +0530

    DVR simplified

--- 6 - DVR/DVR.java
+++ 6 - DVR/DVR.java
@@ -1,19 +1,4 @@
-import java.util.*;
-
-//Note:
-// Initially only the distance to immediate neighbours is only know.
-// We run Bellman For algorithm on each of the node with that node
-// as the source to determine the shortest distance to other nodes.
-
-class RoutingEntry {
-	public int outgoinig;
-	public int distance;
-
-	public RoutingEntry(int outgoing, int distance) {
-		this.outgoinig = outgoing;
-		this.distance = distance;
-	}
-}
+import java.util.Scanner;
 
 public class DVR {
 
@@ -21,62 +6,50 @@ public class DVR {
 		System.out.print(o);
 	}
 
-	// For undirected graphs
-	private static void bellmanFord(int node_count, int source, int graph[][], RoutingEntry routingTable[][]) {
-		int distance[] = new int[node_count];
+	private static void bellmanFord(int node_count, int source, int graph[][], int routingTable[][]) {
+		final int infinity = Integer.MAX_VALUE / 2;
 		for (int i = 0; i < node_count; i++)
-			distance[i] = Integer.MAX_VALUE / 2;
-		distance[source] = 0;
+			routingTable[source][i] = infinity;
+		routingTable[source][source] = 0;
+
 		for (int k = 0; k < node_count - 1; k++)
 			for (int i = 0; i < node_count; i++)
 				for (int j = 0; j < node_count; j++)
 					if (i != j && graph[i][j] != 0)
-						if (distance[j] > distance[i] + graph[i][j]) {
-							distance[j] = distance[i] + graph[i][j];
-							//routingTable[i][j].outgoinig = i;
-						}
-		for (int i = 0; i < node_count; i++)
-			routingTable[source][i].distance = distance[i];
+						if (routingTable[source][j] > routingTable[source][i] + graph[i][j])
+							routingTable[source][j] = routingTable[source][i] + graph[i][j];
 
 	}
 
 	public static void main(String args[]) {
-		int v, neighbour_count;
-		int neighbour, weight;
 		Scanner sc = new Scanner(System.in);
+		int n, neighbour_count;
+		int graph[][], routingTable[][];
 
 		print("Enter the number of nodes:");
-		v = sc.nextInt();
-		int graph[][] = new int[v][v];
-		RoutingEntry routingTable[][] = new RoutingEntry[v][v];
-		for (int i = 0; i < v; i++)
-			for (int j = 0; j < v; j++)
-				routingTable[i][j] = new RoutingEntry(-1, -1);
+		n = sc.nextInt();
+		graph = new int[n][n];
+		routingTable = new int[n][n];
 
-		for (int i = 0; i < v; i++) {
-			print("Enter the number of neighbours of " + i + ": ");
+		for (int i = 0; i < n; i++) {
+			print("\nEnter the number of neighbours of " + i + " :");
 			neighbour_count = sc.nextInt();
-			print("Enter the neighbours and weights:");
+			print("Enter the neigbour and weight pairs:");
 			for (int j = 0; j < neighbour_count; j++) {
-				neighbour = sc.nextInt();
-				weight = sc.nextInt();
+				int neighbour = sc.nextInt();
+				int weight = sc.nextInt();
 				graph[i][neighbour] = weight;
-				graph[neighbour][i] = weight;
-				routingTable[i][neighbour].distance = weight;
-				routingTable[i][neighbour].outgoinig = neighbour;
 			}
 		}
 
-		for (int i = 0; i < v; i++)
-			bellmanFord(v, i, graph, routingTable);
+		for (int i = 0; i < n; i++)
+			bellmanFord(n, i, graph, routingTable);
 
-		for (int i = 0; i < v; i++) {
-			print("\nVector of " + i + ":\n");
-			print("Node Distance\n");
-			for (int j = 0; j < v; j++)
-				System.out.printf("%4d%9d\n", j, routingTable[i][j].distance);
+		print("Shortest Distances:\n");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+				print(routingTable[i][j] + " ");
+			print("\n");
 		}
-
-		sc.close();
 	}
 }
